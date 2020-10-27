55deb4b261d3ec9cbc329d0dd70ba49d5d393fe9
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2020-10-27 17:57:04 -0300

    orphanage-spot.css has been done

---
+++ public/scripts/orphanage-spot.js
@@ -0,0 +1,33 @@
+//disable zoom
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+
+}
+
+
+// create map
+const map = L.map('mapid', options).setView([-23.5503558,-46.6380009], 16);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: `./public/images/map-marker.svg`,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [160, 2]
+
+});
+
+
+//create and add marker
+
+L
+.marker([-23.5503558,-46.6380009],{icon})
+.addTo(map)
+
