a8174aab6a33f4f13ee08aad6fb25b98aaa0508d
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2020-11-02 18:09:07 -0300

    template engine hbs has been configurated

--- src/server.js
+++ src/server.js
@@ -7,11 +7,21 @@ const server = express();
 server.listen(5500);
 //criando rotas para arquivos estáticos
 server.use(express.static('public'));
+//configurar template engine(permie mudar o html de forma dinâmica, com objetos, loops, etc)
+server
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'hbs')
 
 
 // criando rotas
 server.get('/', (req, resp)=>{
     //a variavel __dirname é o caminho da pasta atual
     //devolve a página page-landing.html
-    return resp.sendFile(path.join(__dirname, 'views', 'page-landing.html')); 
+    return resp.render('page-landing'); 
+    /*
+    req é uma parametro que vai pegar dados vindo do cliente
+    pode ser na URL após ? req.query
+    pode ser dados vindo de um formulário req.body
+    esses dados vem em forma de objeto
+    */
 });
