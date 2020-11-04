6e3c888d8aaf437a7ba9fd8ff9d089b9e0c06385
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2020-11-04 17:23:21 -0300

    db.js has been created

--- public/scripts/location-orphanages.js
+++ public/scripts/location-orphanages.js
@@ -1,6 +1,6 @@
 
 // create map
-const map = L.map('mapid').setView([-23.5503558,-46.6380009], 16);
+const map = L.map('mapid').setView([-23.5503558,-46.6380009], 14);
 
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -13,22 +13,43 @@ const icon = L.icon({
     popupAnchor: [160, 2]
 
 });
-
-// create popup overlay
-const popup = L.popup({
+//pega cada propriedade do objeto orphanage, e separa em um parâmetro
+//cria um pin no mapa
+function addMarker({id, name, lat, lng}){
+    // create popup overlay
+    const popup = L.popup({
     closeButton: false,
     className: 'map-popup',
     minWidth: 200,
     minHeight: 100
-}).setContent(`
-    Lar das meninas 
-    <a href="spot-orphanage.html?id=1" class="choose-orphanage">
+    })
+    .setContent(`
+    ${name} 
+    <a href="/spot-orphanage?id=${id}">
     <img src="/images/arrow-white.svg">
     </a>`);
 
 //Pop-up do mapa 
 
-L
-.marker([-23.5503558,-46.6380009],{icon})
-.addTo(map)
-.bindPopup(popup)
+    L
+    .marker([lat, lng], {icon})
+    .addTo(map)
+    .bindPopup(popup)
+
+}
+//pega a lista de objetos do datafake.js pelos spans de location-orphanages,
+const spanOrphanages = document.querySelectorAll('.orphanages span');
+//para cada item lista cria um novo objeto
+spanOrphanages.forEach((span)=>{
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    };
+    //executa a função que cria um pin no mapa
+    addMarker(orphanage);
+});
+
+
+
---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+    { 
+        id: 1,
+        lat: '-23.5503558',
+        lng: '-46.6380009',
+        name: 'Lar das Meninas', 
+        description: 'Presta assistência a crianças de 6 a 15 anos que se encontre em situação de risco e/ou de vulnerabilidade social.', 
+        images: [
+            'https://images.unsplash.com/photo-1594925782033-0238ef32fca0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+
+            'https://images.unsplash.com/photo-1597385573259-d2d9f0b7135d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9'
+        ],
+        instructions: 'Venha quando se sentir a vontade e traga muito amor e paciência para dar.',
+        opening_hours: 'Horário de visitas Das 8h as 18h',
+        open_on_weekends: '1'
+    },
+
+    { 
+        id: 2,
+        lat: '-23.5513558',
+        lng: '-46.6490009',
+        name: 'Umbrella Academy', 
+        description: 'lar de crianças de dons extraordinários,', 
+        images: [
+            'https://images.unsplash.com/photo-1594925782033-0238ef32fca0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+
+            'https://images.unsplash.com/photo-1597385573259-d2d9f0b7135d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9'
+        ],
+        instructions: 'Venha quando se sentir a vontade e traga muito amor e paciência para dar.',
+        opening_hours: 'Horário de visitas Das 8h as 18h',
+        open_on_weekends: '0'
+    },
+    
+]
